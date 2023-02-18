8183584a9bcae5901c57f5d55c050f2d5cf0c35f
ismb99 <im_077@hotmail.com>
2023-02-18 09:33:07 +0100

    added files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Travel from "./Travel";
+
+export default function App() {
+  return (
+    <div>
+      <Travel title="Dubai Lyx hotell" />
+    </div>
+  );
+}
---
+++ src/Travel.jsx
@@ -0,0 +1,9 @@
+export default function Travel(props) {
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <img src="./src/assets/dubai.jpg" alt="dubai hotel" />
+      <p>View on Google Maps</p>
+    </div>
+  );
+}
